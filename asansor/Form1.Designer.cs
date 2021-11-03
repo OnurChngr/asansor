d590d7d3e2a2c5c9d527262615f4bac41ec73057
l6_pc2 <l6_pc2@WIN-IM38HI1GTD6>
2021-11-03 13:17:27 +0300

    Add project files.

---
+++ asansor/Form1.Designer.cs
@@ -0,0 +1,245 @@
+﻿
+namespace asansor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn4kat = new System.Windows.Forms.Button();
+            this.btn3kat = new System.Windows.Forms.Button();
+            this.btn2kat = new System.Windows.Forms.Button();
+            this.btn1kat = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblAsansor1 = new System.Windows.Forms.Label();
+            this.lblAsansor2 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtBulKat = new System.Windows.Forms.TextBox();
+            this.btnCagir = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lblGosterAsansor1 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.lblGosterAsansor2 = new System.Windows.Forms.Label();
+            this.lblGeliyor = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn4kat
+            // 
+            this.btn4kat.Location = new System.Drawing.Point(350, 12);
+            this.btn4kat.Name = "btn4kat";
+            this.btn4kat.Size = new System.Drawing.Size(75, 38);
+            this.btn4kat.TabIndex = 0;
+            this.btn4kat.Text = "4.kat";
+            this.btn4kat.UseVisualStyleBackColor = true;
+            // 
+            // btn3kat
+            // 
+            this.btn3kat.Location = new System.Drawing.Point(350, 56);
+            this.btn3kat.Name = "btn3kat";
+            this.btn3kat.Size = new System.Drawing.Size(75, 34);
+            this.btn3kat.TabIndex = 1;
+            this.btn3kat.Text = "3.Kat";
+            this.btn3kat.UseVisualStyleBackColor = true;
+            // 
+            // btn2kat
+            // 
+            this.btn2kat.Location = new System.Drawing.Point(350, 96);
+            this.btn2kat.Name = "btn2kat";
+            this.btn2kat.Size = new System.Drawing.Size(75, 37);
+            this.btn2kat.TabIndex = 2;
+            this.btn2kat.Text = "2.Kat";
+            this.btn2kat.UseVisualStyleBackColor = true;
+            // 
+            // btn1kat
+            // 
+            this.btn1kat.Location = new System.Drawing.Point(350, 139);
+            this.btn1kat.Name = "btn1kat";
+            this.btn1kat.Size = new System.Drawing.Size(75, 31);
+            this.btn1kat.TabIndex = 3;
+            this.btn1kat.Text = "1.Kat";
+            this.btn1kat.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(261, 208);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "1. Asansör:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(399, 208);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "2. Asansör";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // lblAsansor1
+            // 
+            this.lblAsansor1.AutoSize = true;
+            this.lblAsansor1.Location = new System.Drawing.Point(330, 208);
+            this.lblAsansor1.Name = "lblAsansor1";
+            this.lblAsansor1.Size = new System.Drawing.Size(37, 13);
+            this.lblAsansor1.TabIndex = 6;
+            this.lblAsansor1.Text = "..........";
+            this.lblAsansor1.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // lblAsansor2
+            // 
+            this.lblAsansor2.AutoSize = true;
+            this.lblAsansor2.Location = new System.Drawing.Point(472, 208);
+            this.lblAsansor2.Name = "lblAsansor2";
+            this.lblAsansor2.Size = new System.Drawing.Size(40, 13);
+            this.lblAsansor2.TabIndex = 7;
+            this.lblAsansor2.Text = "...........";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(261, 252);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(97, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Bulunduğunuz Kat:";
+            // 
+            // txtBulKat
+            // 
+            this.txtBulKat.Location = new System.Drawing.Point(402, 245);
+            this.txtBulKat.Name = "txtBulKat";
+            this.txtBulKat.Size = new System.Drawing.Size(100, 20);
+            this.txtBulKat.TabIndex = 9;
+            // 
+            // btnCagir
+            // 
+            this.btnCagir.Location = new System.Drawing.Point(350, 314);
+            this.btnCagir.Name = "btnCagir";
+            this.btnCagir.Size = new System.Drawing.Size(75, 33);
+            this.btnCagir.TabIndex = 10;
+            this.btnCagir.Text = "Çağır";
+            this.btnCagir.UseVisualStyleBackColor = true;
+            this.btnCagir.Click += new System.EventHandler(this.btnCagir_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(261, 368);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(60, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "1. Asansör:";
+            // 
+            // lblGosterAsansor1
+            // 
+            this.lblGosterAsansor1.AutoSize = true;
+            this.lblGosterAsansor1.Location = new System.Drawing.Point(347, 367);
+            this.lblGosterAsansor1.Name = "lblGosterAsansor1";
+            this.lblGosterAsansor1.Size = new System.Drawing.Size(31, 13);
+            this.lblGosterAsansor1.TabIndex = 12;
+            this.lblGosterAsansor1.Text = "........";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(420, 368);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(60, 13);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "2. Asansör:";
+            // 
+            // lblGosterAsansor2
+            // 
+            this.lblGosterAsansor2.AutoSize = true;
+            this.lblGosterAsansor2.Location = new System.Drawing.Point(485, 367);
+            this.lblGosterAsansor2.Name = "lblGosterAsansor2";
+            this.lblGosterAsansor2.Size = new System.Drawing.Size(34, 13);
+            this.lblGosterAsansor2.TabIndex = 14;
+            this.lblGosterAsansor2.Text = ".........";
+            // 
+            // lblGeliyor
+            // 
+            this.lblGeliyor.AutoSize = true;
+            this.lblGeliyor.Location = new System.Drawing.Point(347, 281);
+            this.lblGeliyor.Name = "lblGeliyor";
+            this.lblGeliyor.Size = new System.Drawing.Size(61, 13);
+            this.lblGeliyor.TabIndex = 15;
+            this.lblGeliyor.Text = "..................";
+            this.lblGeliyor.Click += new System.EventHandler(this.lblGeliyor_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblGeliyor);
+            this.Controls.Add(this.lblGosterAsansor2);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.lblGosterAsansor1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.btnCagir);
+            this.Controls.Add(this.txtBulKat);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lblAsansor2);
+            this.Controls.Add(this.lblAsansor1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn1kat);
+            this.Controls.Add(this.btn2kat);
+            this.Controls.Add(this.btn3kat);
+            this.Controls.Add(this.btn4kat);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn4kat;
+        private System.Windows.Forms.Button btn3kat;
+        private System.Windows.Forms.Button btn2kat;
+        private System.Windows.Forms.Button btn1kat;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblAsansor1;
+        private System.Windows.Forms.Label lblAsansor2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtBulKat;
+        private System.Windows.Forms.Button btnCagir;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lblGosterAsansor1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label lblGosterAsansor2;
+        private System.Windows.Forms.Label lblGeliyor;
+    }
+}
+
---
+++ asansor/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace asansor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int a1, a2, s1, s2,random;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Random rastgele = new Random(); // rastgele sayı üretecek olan nesne
+            a1 = rastgele.Next(1, 5);
+             a2 = rastgele.Next(1, 5);
+            random = rastgele.Next(0, 2);
+
+            lblAsansor1.Text = a1.ToString();
+            lblAsansor2.Text = a2.ToString();
+
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblGeliyor_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCagir_Click(object sender, EventArgs e)
+        {
+           
+            int kat = int.Parse(txtBulKat.Text);
+            s1 = kat - a1;
+            s2 = kat - a2;
+            if (s1 > s2)
+            {
+                a1 = kat;
+                lblGeliyor.Text = "Asansör Geliyor Lütfen Bekleyiniz";
+            }
+            else if (s2 > s1)
+            {
+                a2 = kat;
+                lblGeliyor.Text = "Asansör Geliyor Lütfen Bekleyiniz";
+            }
+            else
+            {
+                a1 = kat;
+            }
+            //lblGosterAsansor2.Text = kat.ToString();
+
+
+
+
+        }
+    }
+}
